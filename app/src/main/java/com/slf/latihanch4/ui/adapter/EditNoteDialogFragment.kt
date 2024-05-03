24ea450f61109d86c1518cbca72bceec1f0db9e4
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-05-03 15:48:03 +0700

    fix : view edit xml

--- app/src/main/java/com/slf/latihanch4/ui/adapter/EditNoteDialogFragment.kt
+++ app/src/main/java/com/slf/latihanch4/ui/adapter/EditNoteDialogFragment.kt
@@ -2,14 +2,15 @@ package com.slf.latihanch4.ui.adapter
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import com.slf.latihanch4.R
-import android.view.LayoutInflater
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import com.slf.latihanch4.R
 import com.slf.latihanch4.data.model.Note
 import com.slf.latihanch4.ui.dashboard.DashboardViewModel
 import kotlinx.coroutines.launch
@@ -21,32 +22,38 @@ class EditNoteDialogFragment(private val note: Note) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            val inflater = requireActivity().layoutInflater;
+            val inflater = requireActivity().layoutInflater
             val view = inflater.inflate(R.layout.fragment_edit_note, null)
 
             val titleEditText = view.findViewById<EditText>(R.id.editTextTitle)
             val contentEditText = view.findViewById<EditText>(R.id.editTextContent)
-
             titleEditText.setText(note.title)
             contentEditText.setText(note.content)
 
-            builder.setView(view)
-                .setPositiveButton("Save") { _, _ ->
-                    val title = titleEditText.text.toString()
-                    val content = contentEditText.text.toString()
+            builder.setView(view) // Menambahkan tampilan ke pembangun dialog
 
-                    // Handle save note action
-                    lifecycleScope.launch {
-                        viewModel.update(Note(note.id, title, content))
-                    }
-                    Toast.makeText(context, "Note saved: $title, $content", Toast.LENGTH_SHORT).show()
-                }
-                .setNegativeButton("Cancel") { _, _ ->
-                    dialog?.cancel()
-                }
+            val dialog = builder.create() // Membuat dialog
 
-            builder.create()
+            val saveButton = view.findViewById<Button>(R.id.ok_btn_id)
+            val cancelButton = view.findViewById<ImageButton>(R.id.cancelID)
+
+            saveButton.setOnClickListener {
+                val title = titleEditText.text.toString()
+                val content = contentEditText.text.toString()
+
+                // Handle save note action
+                lifecycleScope.launch {
+                    viewModel.update(Note(note.id, title, content))
+                }
+                Toast.makeText(context, "Note saved: $title, $content", Toast.LENGTH_SHORT).show()
+                dialog.dismiss() // Menutup dialog setelah menyimpan
+            }
+
+            cancelButton.setOnClickListener {
+                dialog.dismiss() // Menutup dialog tanpa menyimpan perubahan
+            }
+
+            dialog // Mengembalikan dialog yang telah dibuat
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
-
